18b412cda22ec6579f7252d1dd9f302387e5a33d
sflabws <107478791+sflabws@users.noreply.github.com>
2022-06-20 16:28:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,14 +24,16 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header('Fruityvice fruit advice')
-fruit_choice=streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered: ',fruit_choice)
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
-
-streamlit.stop()
+try:
+  fruit_choice=streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select the fruit to get information")
+  else:
+    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.stop()
 
 
 
